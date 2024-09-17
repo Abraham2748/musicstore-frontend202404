4e6708c8e9771ec7bc69e0bd07d858535cef6351
Abraham2748 <abraham_jack10@hotmail.com>
2024-09-16 22:28:02 -0500

    Avance clase 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { EventCardComponent } from '../shared/components/event-card/event-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    EventCardComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
