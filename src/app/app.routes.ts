3c61fd38b5617f35614f1f393da32d8c208b6e7e
Abraham2748 <abraham_jack10@hotmail.com>
2024-09-23 20:53:50 -0500

    Maquetación de Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {
@@ -23,5 +24,10 @@ export const routes: Routes = [
     path: 'register',
     pathMatch: 'full',
     component: RegisterComponent,
+  },
+  {
+    path: 'forgot-password',
+    pathMatch: 'full',
+    component: ForgotPasswordComponent,
   },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    SimpleHeaderComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FooterComponent,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
