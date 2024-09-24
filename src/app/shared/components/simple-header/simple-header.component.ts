cf7004d500e9d9c7b38c7ceb760c1dbadaac9c2a
Abraham2748 <abraham_jack10@hotmail.com>
2024-09-23 19:24:17 -0500

    Maquetación e Implementación del Simple Header

--- src/app/shared/components/simple-header/simple-header.component.ts
+++ src/app/shared/components/simple-header/simple-header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-simple-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './simple-header.component.html',
-  styleUrl: './simple-header.component.css'
+  styleUrl: './simple-header.component.css',
 })
-export class SimpleHeaderComponent {
-
-}
+export class SimpleHeaderComponent {}
